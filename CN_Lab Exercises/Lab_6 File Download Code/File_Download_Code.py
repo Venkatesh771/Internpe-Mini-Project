a306f6e525fd47e9689bb90b173fde483b0841ff
Venkateswar Reddy k <126060585+Venkatesh771@users.noreply.github.com>
2023-03-22 18:35:05 +0530

    Delete CN_Lab Exercises directory

--- CN_Lab Exercises/Lab_6 File Download Code/File_Download_Code.py
+++
@@ -1,8 +0,0 @@
-#6.Web HTTP File Download Python Code:
-import requests
-url = "https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&w=600"
-filename = "pic.jpeg"
-response = requests.get(url)
-with open(filename, "wb") as f:
-f.write(response.content)
-print("File downloaded successfully!\nYou Can Check The Folder With File Name As ",filename)
